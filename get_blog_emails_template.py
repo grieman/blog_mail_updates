346f6c6b5e7fcd3b62611368bb09ec82d6f82308
RiemanG <graeham.rieman@controlsdata.com>
2017-06-07 09:44:54 -0500

    Added time conditions

--- get_blog_emails_template.py
+++ get_blog_emails_template.py
@@ -1,14 +1,19 @@
 import imaplib
 import email
+import datetime
+import sys
 
 def get_blog_emails():
 	try:
 		gmail = imaplib.IMAP4_SSL('imap.gmail.com')
-		gmail.login('gnereicusername1@gmail.com', 'password')
+		gmail.login('genereicusername1@gmail.com', 'password')
 		gmail.list()
 		gmail.select('blog_journal')
 		
 		type, data = gmail.search(None, 'ALL')
+		
+		file = open("blog_journal_template.txt", "r")
+		last_date = file.readline().rstrip()
 		
 		for x in range(len(data[0].split())):
 			latest = data[0].split()[x]
@@ -19,8 +24,8 @@ def get_blog_emails():
 			date = msg['Date']
 			
 			## Section to compare date to most recent date, only proceed if more recent
-			## if date <= last_date
-			##     break
+			if datetime.datetime.strptime(date,"%a, %d %b %Y %H:%M:%S %z") <= datetime.datetime.strptime(last_date,"%a, %d %b %Y %H:%M:%S %z")
+			    sys.exit()
 			
             if msg.is_multipart():
                 body = msg.get_payload(0).get_payload()
